84953f6cc1b0d7db621aee10ff8df50efe27020d
Brandon Roberts <brandon.roberts@synapse-wireless.com>
2018-02-01 09:54:48 -0500

    Add talk slides from ng-ATL 2018

--- e2e/app.e2e-spec.ts
+++
@@ -1,9 +0,0 @@
-import * as appPage from './app';
-
-describe('ng-atl App', () => {
-  it('should display welcome message', async() => {
-    await appPage.navigateTo();
-
-    expect(await appPage.getParagraphText().getText()).toEqual('Welcome to app!');
-  });
-});
--- e2e/app.ts
+++
@@ -1,4 +0,0 @@
-import { browser, by, element } from 'protractor';
-
-export const navigateTo = () => browser.get('/');
-export const getParagraphText = () => element(by.css('app-root h1'));
